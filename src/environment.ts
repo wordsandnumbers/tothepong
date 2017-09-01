0275eb3e316508d7a85ee25636dd1632cc93e075
Justin Sawyer <jsawyer@collegenet.com>
2017-09-01 15:52:00 -0700

    changing to tothepong

---
+++ src/environment.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyBTi9LZSls_JKBhN5tuj5ebro40srpjOWU",
+	authDomain: "tothepong.firebaseapp.com",
+	databaseURL: "https://tothepong.firebaseio.com",
+	projectId: "tothepong",
+	storageBucket: "tothepong.appspot.com",
+	messagingSenderId: "344307642072"
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	game: FirebaseObjectObservable<any>;
 
-  }
+	constructor(
+		public db: AngularFireDatabase,
+		public navCtrl: NavController
+	) {
+		this.game = db.object('/game');
+	}
 
 }
