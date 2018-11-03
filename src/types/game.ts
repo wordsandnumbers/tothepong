c28131c654f8fcf2b892c749183efce745d33a20
Justin Sawyer <jsawyer@collegenet.com>
2018-11-02 17:09:41 -0700

    More team score biz

--- src/types/game.ts
+++ src/types/game.ts
@@ -9,40 +9,56 @@ export enum GameState {
 	ARCHIVED
 }
 
+class TeamScore {
+	team: Team;
+	score: number;
+}
+
 export class Game {
 	startDate: string;
 	endDate: string;
 	pointScores: Array<PointScore>;
 	totalPoints: number;
 	state: GameState;
+	readonly teams: Array<Team>;
 
-	get standings(): any {
-		let standings: object = {};
-
-		this.pointScores.forEach((pointScore: PointScore) => {
-			if (standings[pointScore.team.id]) {
-				standings[pointScore.team.id] += pointScore.points;
-			} else {
-				standings[pointScore.team.id] = pointScore.points;
-			}
-		});
-		return standings;
-	}
-
-	public isComplete(): boolean {
-		return !!this.winner();
-	}
-
-	public winner(): Team | undefined {
-		let highTeam: Team;
-		this.standings.forEach((score: number, team: Team) => {
-		});
-		return highTeam;
-	}
-
-	constructor() {
+	constructor(totalPoints: number, teams: Array<Team>) {
 		this.startDate = new Date().toISOString();
 		this.pointScores = new Array<PointScore>();
 		this.state = GameState.NEW;
+		this.totalPoints = totalPoints;
+		this.teams = teams;
 	}
+
+	get score() {
+		let score: any = {};
+
+		this.pointScores.forEach((pointScore: PointScore) => {
+			if (score[pointScore.team.id]) {
+				score[pointScore.team.id] += pointScore.points;
+			} else {
+				score[pointScore.team.id] = pointScore.points;
+			}
+		});
+
+		return Object.keys(score).reduce((scores, key) => {
+			scores.push({team: key, score: score[key]});
+			return scores;
+		}, []);
+	}
+
+	public isComplete(): boolean {
+		return !!this.winner;
+	}
+
+	get winner(): TeamScore {
+		let sorted = this.score.sort((a: TeamScore, b: TeamScore) => a.score - b.score);
+		let highTeam = sorted[sorted.length - 1];
+		let nextTeam = sorted[sorted.length - 2];
+
+		if (highTeam && nextTeam && highTeam.score >= this.totalPoints && highTeam.score - nextTeam.score >= 2) {
+			return highTeam;
+		}
+	}
+
 }
--- src/types/match.ts
+++ src/types/match.ts
@@ -20,26 +20,32 @@ export class Match {
 	totalGames: number;
 	$key?: number;
 
-	addScore(team: Team, pointAmount?: number) {
-		this.games[this.games.length - 1].pointScores.push(new PointScore(team));
-	}
-
-	subtractScore(team: Team): void {
-		let currentGame = this.games[this.games.length - 1];
-		let foundIndex = currentGame.pointScores.findIndex((pointScore: PointScore) => {
-			return pointScore.team.id === team.id;
-		});
-		if (foundIndex > -1) {
-			currentGame.pointScores.splice(foundIndex, 1);
-		}
-	}
-
 	constructor(totalGames: number, teams: Array<Team>) {
 		this.games = [];
-		this.games.push(new Game());
+		this.games.push(new Game(11, teams));
 		this.startDate = new Date();
 		this.state = MatchState.NEW;
 		this.teams = teams;
 		return this;
 	}
+
+	addScore(team: Team, pointAmount?: number) {
+		this.games[this.games.length - 1].pointScores.push(new PointScore(team));
+	}
+
+	get currentGame(): Game {
+		return this.games[this.games.length - 1];
+	}
+
+	subtractScore(team: Team): Match {
+		let foundIndex = this.currentGame.pointScores.reverse().findIndex((pointScore: PointScore) => {
+			return pointScore.team.id === team.id;
+		});
+		if (foundIndex > -1) {
+			this.currentGame.pointScores.splice(foundIndex, 1);
+		}
+		this.currentGame.pointScores.reverse();
+		return this;
+	}
+
 }
